a94aea864b10b02d68f8ca78ac1fc89fe40399e8
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-11-04 20:25:42 -0300

    requisito 4

--- script.js
+++ script.js
@@ -2,3 +2,23 @@ const section = document.getElementsByTagName('section')[0];
 const button = document.createElement('button');
 button.innerText = 'Cores aleatórias';
 section.appendChild(button);
+
+function generateColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let index = 0; index < 6; index += 1) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function setColor(){
+ const classeColor = document.getElementsByClassName('color');
+ for(index = 1; index < classeColor.length; index += 1){
+ classeColor[index].style.backgroundColor = generateColor(); 
+ }
+
+}
+
+
+button.addEventListener('click', setColor);
