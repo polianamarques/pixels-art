2eb5f96f81f43ba6dc3b8365682c998fec8355b6
contatopolianamarques@gmail.com <contatopolianamarques@gmail.com>
2022-11-07 16:27:31 -0300

    requisito 09

--- script.js
+++ script.js
@@ -13,21 +13,28 @@ function generateColor() {
   return color;
 }
 
-function setColor(){
+function setColor() {
   const classeColor = document.getElementsByClassName('color');
- for(index = 1; index < classeColor.length; index += 1){
+ for (index = 1; index < classeColor.length; index += 1) {
  classeColor[index].style.backgroundColor = generateColor(); 
  }
-
 }
 button.addEventListener('click', setColor);
 
 const pixelBoard = document.getElementById('pixel-board');
-for(let index = 0; index < 25; index +=1){
+for (let index = 0; index < 25; index += 1) {
 const individualPixel = document.createElement('div');
-pixelBoard. appendChild(individualPixel).className = 'pixel';
+pixelBoard.appendChild(individualPixel).className = 'pixel';
 }
 
-function 
+ const color = document.getElementsByClassName('color');
+ function addAndRmvSelector(event){
+  const selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
+  }
+  
+   for(let colorsOf of color){
+    colorsOf.addEventListener('click', addAndRmvSelector);
+   }
  
-}
